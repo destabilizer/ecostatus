5849d0214851b40ccaf7ac45024d87060b006e78
Balaram Usov <dest@disroot.org>
2019-05-13 17:08:49 +0300

    Added real source

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from server import initserver
 def main():
     serv = initserver()
     serv.initDataStack()
-    serv.datastack().newSource("test")
+    serv.datastack().newSource("lattepanda1")
+    serv.datastack().newSource("testclient")
     serv.database().create_with_timestamp()
     serv.serve_forever()
 
